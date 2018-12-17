7fafdb9acb2dd776ce796f7f6e6f8d3067b772c3
Fernando Herrera <fherrera2018@gmail.com>
2018-12-16 19:52:17 -0800

    Added Jumbo.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 // My Imports start here:
 import NavigationBar from "./Components/NavigationBar";
+import Jumbo from "./Components/Jumbo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavigationBar />
+        <Jumbo />
       </div>
     );
   }
