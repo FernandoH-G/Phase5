5ec8c3b16c3229c0550a8c6e8e7394e26e0bf1b8
Fernando Herrera <fherrera2018@gmail.com>
2018-12-19 01:08:34 -0800

    Integrated Express. Added Express components in /react-backend. Integrated nodemon to actively check for changes to API. Added react-backend/node_modules to .gitignore

--- src/App.js
+++ src/App.js
@@ -10,14 +10,24 @@ import DistributionEvents from "./Routes/DistributionEvents";
 import About from "./Routes/About";
 
 class App extends Component {
+  state = { users: [] };
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <NavigationBar />
           <Jumbo />
+          <h1>Users</h1>
+          {this.state.users.map(user => (
+            <div key={user.id}>{user.username}</div>
+          ))}
           <Switch>
-            {/* <Route path="./" component={App} /> */}
+            <Route exact path="/" />
             <Route path="/distributionevents" component={DistributionEvents} />
             <Route path="/about" component={About} />
           </Switch>
--- src/Routes/DistributionEvents.js
+++ src/Routes/DistributionEvents.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import MainArea from "../Components/MainArea";
 import JumboDE from "../Components/JumboDE";
+// import {} from "pg";
+// var pg = require("pg");
+// var exp = require("express");
 
 class DistributionEvents extends Component {
   render() {
